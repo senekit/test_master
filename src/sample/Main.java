414045c4b82e1808c12549a1d31e3ad9aae97351
HP <1226023416@qq.com>
2020-09-15 19:40:03 +0800

    aaaaaaaaaaaaaaa

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        //wssb
+        //      wssb
+        //              wssb
+        //
+        //
+        //
+        //
+        //
     }
-    //wssbweqweqeqweqweqwe
 
     public static void main(String[] args) {
         launch(args);
