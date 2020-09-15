a8e4087a020ece12f1f83cf0ef9ebf9ecfb7b691
Booker-100 <904677070@qq.com>
2020-09-15 19:44:28 +0800

    好耶！

--- src/sample/App.java
+++ src/sample/App.java
@@ -5,5 +5,6 @@ import javafx.application.Application;
 public class App {
     public static void main(String[] args) {
         Application.launch(Main.class);
+        system.out.printlln("ye!");
     }
 }
